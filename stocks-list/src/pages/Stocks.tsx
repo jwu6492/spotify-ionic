b89fd4d66fc4a35e7ccffbef45650457aed27644
Nithin <ndaniel36@gatech.edu>
2022-09-11 20:48:03 -0400

    added error handling for searching stock

--- stocks-list/src/pages/Stocks.tsx
+++ stocks-list/src/pages/Stocks.tsx
@@ -14,14 +14,21 @@ import axios from "axios";
 import http from "http";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Tab1.css";
+import { toast } from "../toast"
 
 const Stocks: React.FC = () => {
   const [stock, setStock] = useState("");
   const [price, setPrice] = useState(null);
   async function stockSearch() {
     const res = await axios.get(`https://api.polygon.io/v2/aggs/ticker/${stock}/prev?adjusted=true&apiKey=N0xYH0QttgysqIJbFSkIt6xAn1OqefEt`);
+    if (res.status != 200) {
+      toast("Could not retreive stock data, please make sure you entered in the symbol correctly")
+    }
     if (res.data != null) {
       setPrice(res.data.results[0].c);
+      toast("Successfully search for the stock");
+    } else {
+      toast("Unsuccess search for the stock");
     }
   }
   return (
